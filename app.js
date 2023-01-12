6993a0d4706ec17076c1575ee671842dced040fe
samuel sholib <samuelsholib12@gmail.com>
2023-01-11 18:09:04 -0800

    configuration

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-import express  from "express";
+import express from "express";
+import dotenv from "dotenv";
+import { Path } from "mongoose";
 const app = express();
 
+import { db } from "./config/connection.js";
 
-app.listen(3001, () => {
-    console.log ("App listening at port 3001.")
-})
+db.once('open', () => {
+
+
+    app.listen(3001, () => {
+        connection()
+        console.log("App listening at port 3001.")
+    })
+});
