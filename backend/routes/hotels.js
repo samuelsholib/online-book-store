75ecec9dc10e94c76518a0cc293239ef6f29f4c9
samuelsholib <samuelsholib12@gmail.com>
2023-01-14 13:45:04 -0800

    routes

---
+++ backend/routes/hotels.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+const router = express.Router();
+
+
+//Routes 
+//GetRoute
+router.get("/get", (req, res) => {
+
+})
+//getALL
+router.get("/getAll", (req, res) => {
+    
+})
+
+//PostRoute
+router.post("/addNew", (req, res) => {
+    
+})
+//DeleteRoute
+router.delete("/delete", (req, res) => {
+    
+})
+//UpdateRoute
+router.put("/update", (req, res) => {
+    
+})
+
+
+export default router;
